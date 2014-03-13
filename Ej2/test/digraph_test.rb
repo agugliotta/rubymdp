ce745542535a750bd19ba2a61d4d8335e1da7ddb
Agustin Gugliotta <agustin.mdq.89@gmail.com>
2014-03-13 14:07:06 -0300

    Gemfile added

---
+++ Ej2/test/digraph_test.rb
@@ -0,0 +1,43 @@
+require_relative 'minitest_helper'
+require 'digraph.rb'
+
+class DiagraphTest < Minitest::Test
+
+    def test_show
+        g = Digraph.new
+        g.push_edge("A", "B", "50")
+        g.push_edge("A", "B", "300")
+        g.push_edge("A", "C", "175")
+        g.push_edge("B", "C", "75")
+        g.push_edge("B", "Z", "250")
+        g.push_edge("C", "B", "50")
+        g.push_edge("C", "Z", "100")
+        output = ["A", "B", "C", "Z"]
+        assert_equal output, g.show
+    end
+end
+
+class EdgeTest < Minitest::Test
+
+    def test_size
+        e = Edge.new('A')
+        e.push_neighbor('B',5)
+        output = 1
+        assert_equal output, e.size
+
+        e.push_neighbor('C',5)
+        output = 2
+        assert_equal output, e.size
+    end
+
+    def test_has_destinations?
+        e = Edge.new('A')
+        e.push_neighbor('B',5)
+        output = true
+        assert_equal output, e.has_destinations?
+
+        e = Edge.new('A')
+        output = false
+        assert_equal output, e.has_destinations?
+    end
+end
